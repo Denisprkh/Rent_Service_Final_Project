a4cff499fbb05ddd0bb78b3b52d2b4ee6c1c185d
Denis Prokhorenko <depofficial@mail.ru>
2020-09-08 20:29:27 +0300

    commit

--- src/main/java/by/prokhorenko/rentservice/controller/command/impl/ChangeLanguageCommand.java
+++ src/main/java/by/prokhorenko/rentservice/controller/command/impl/ChangeLanguageCommand.java
@@ -5,13 +5,10 @@ import by.prokhorenko.rentservice.controller.command.Attribute;
 import by.prokhorenko.rentservice.controller.command.Command;
 import by.prokhorenko.rentservice.controller.command.RequestParameter;
 import by.prokhorenko.rentservice.controller.command.util.CommandUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class ChangeLanguageCommand implements Command {
-    private static final Logger LOG = LogManager.getLogger();
 
     @Override
     public Router execute(HttpServletRequest request) {
@@ -20,7 +17,6 @@ public class ChangeLanguageCommand implements Command {
         if (targetLanguage != null) {
             request.getSession().setAttribute(Attribute.LANGUAGE, targetLanguage);
             previousPage = request.getHeader(CommandUtil.REFERER_HEADER);
-            LOG.debug(previousPage);
         }
         return new Router(previousPage);
     }
